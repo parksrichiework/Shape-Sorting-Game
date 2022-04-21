8d108d96592ca8299652bf58e857af57be85cb6e
Richie Parks <parksrichiework@gmail.com>
2022-04-21 18:19:04 -0500

    brought the game to life
    javascript: alert when matches are right or wrong
    css: made chosen shapes dance

--- js/main.js
+++ js/main.js
@@ -3,34 +3,82 @@
     const triangle= document.querySelector('#triangle')
     const shapes= document.querySelectorAll('.btm')
 
-    Array.from(shapes).forEach(element => element.addEventListener('click', log))
+    Array.from(shapes).forEach(element => element.addEventListener('click', pick)) 
 
 
-    function log(click){
+    function pick(click){
  
        if(click.target.classList.contains('triangle1')){
         document.querySelector('#empty-triangle').classList.toggle('hidden')
         document.querySelector('#empty-square').classList.add('hidden')
         document.querySelector('#empty-circle').classList.add('hidden')
-        console.log("Triangle")
+        document.querySelector('#triangle').classList.toggle('chosen')
+        document.querySelector('#square').classList.remove('chosen')
+        document.querySelector('#circle').classList.remove('chosen')
+        console.log("Triangle Selected")
        }       
 
        if(click.target.classList.contains('square1')){
-        document.querySelector('#empty-square').classList.toggle('hidden')
-        document.querySelector('#empty-triangle').classList.add('hidden')
+        document.querySelector('#empty-square').classList.toggle('hidden')        
+        document.querySelector('#empty-triangle').classList.add('hidden')        
         document.querySelector('#empty-circle').classList.add('hidden')
-        console.log('Square')
+        document.querySelector('#triangle').classList.remove('chosen')
+        document.querySelector('#circle').classList.remove('chosen')
+        document.querySelector('#square').classList.toggle('chosen')
+        console.log('Square selected')
        }
 
        if(click.target.classList.contains('circle1')){
         document.querySelector('#empty-circle').classList.toggle('hidden')
         document.querySelector('#empty-square').classList.add('hidden')
         document.querySelector('#empty-triangle').classList.add('hidden')
-        console.log('Circle')
+        document.querySelector('#square').classList.remove('chosen')
+        document.querySelector('#triangle').classList.remove('chosen')
+        // document.querySelector('#circle').classList.add('chosen')
+        document.querySelector('#circle').classList.toggle('chosen')
+        console.log('Circle selected')
        }
 
 
     }
+
+    const emptyCircle= document.querySelector('#emptyCircle')
+    const emptySquare= document.querySelector('#emptySquare')
+    const emptyTriangle= document.querySelector('#emptyTriangle')
+    const emptyShapes= document.querySelectorAll('.top-shapes')
+
+    Array.from(emptyShapes).forEach(element => element.addEventListener('click', match))
+
+
+    function match(click){
+
+      if (click.target.classList.contains('emptyTriangle') && document.querySelector('#triangle').classList.contains('chosen')){
+         alert('Correct!')
+      } else if (click.target.classList.contains('emptyTriangle') && document.querySelector('#square').classList.contains('chosen')){
+         alert('Oops! Try Again!')
+      } else if (click.target.classList.contains('emptyTriangle') && document.querySelector('#circle').classList.contains('chosen')){
+         alert('Almost! Try again!')
+      }
+
+      if (click.target.classList.contains('emptySquare') && document.querySelector('#square').classList.contains('chosen')){
+         alert('Correct!')
+      } else if (click.target.classList.contains('emptySquare') && document.querySelector('#triangle').classList.contains('chosen')){
+         alert('Oops! Try Again!')
+      } else if (click.target.classList.contains('emptySquare') && document.querySelector('#circle').classList.contains('chosen')){
+         alert('Almost! Try again!')
+      }
+
+      if (click.target.classList.contains('emptyCircle') && document.querySelector('#circle').classList.contains('chosen')){
+         alert('Correct!');
+         document.querySelector('#empty-circle').classList.toggle('solved')
+
+      } else if (click.target.classList.contains('emptyCircle') && document.querySelector('#square').classList.contains('chosen')){
+         alert('Oops! Try Again!')
+      } else if (click.target.classList.contains('emptyCircle') && document.querySelector('#triangle').classList.contains('chosen')){
+         alert('Almost! Try again!')
+      }
+    }
+
 
 
 
